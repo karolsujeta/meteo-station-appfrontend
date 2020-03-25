1c2d29d576f0ec36f2ffe7733b9e366d69f02b0e
Karol Sujeta <karol.sujeta@outlook.com>
2020-03-25 11:00:40 +0100

    v4

--- src/app/components/map/map-component.component.ts
+++ src/app/components/map/map-component.component.ts
@@ -1,35 +1,72 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
+declare var ol: any;
+declare var map: any;
 
 @Component({
   selector: 'app-map-component',
   templateUrl: './map-component.component.html',
   styleUrls: ['./map-component.component.css']
 })
-export class MapComponentComponent implements AfterViewInit {
+
+export class MapComponentComponent implements OnInit {
 
   private map;
 
   constructor() { }
 
-  ngAfterViewInit(): void {
-    this.initMap();
-  }
+  ngOnInit() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([23.1688403, 53.1324886]),
+        zoom: 8,
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [53.1324886, 23.1688403],
-      zoom: 9
+      })
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    this.map.on('click', function (args) {
+      console.log(args.coordinate);
+      var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
+      console.log(lonlat);
+
+      var lon = lonlat[0];
+      var lat = lonlat[1];
+      alert(`lat: ${lat} long: ${lon}`);
     });
-
-    tiles.addTo(this.map);
   }
-
-
 }
+
+
+
+// -----PIERWSZA WERSJA MAPY-----//
+// export class MapComponentComponent implements AfterViewInit {
+
+//   private map;
+
+//   constructor() { }
+
+//   ngAfterViewInit(): void {
+//     this.initMap();
+//   }
+
+//   private initMap(): void {
+//     this.map = L.map('map', {
+//       center: [53.1324886, 23.1688403],
+//       zoom: 9
+//     });
+
+//     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//       maxZoom: 19,
+//       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//     });
+
+//     tiles.addTo(this.map);
+//   }
+// }
