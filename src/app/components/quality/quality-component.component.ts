76d638f49739ad366ee0dbb68a92e2c72f2166f8
Szymon Borodziuk <szymon671@gmail.com>
2020-04-06 10:26:12 +0200

    dodanie moliwoci otrzymania wyników o jakoci smogu bezporednio po klikniciu na mape

--- src/app/components/quality/quality-component.component.ts
+++ src/app/components/quality/quality-component.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
-import { QualityServiceService } from 'src/app/services/quality/quality-service.service';
+import { QualityServiceService } from '../../services/quality/quality-service.service';
+// import { QualityServiceService } from 'src/app/services/quality/quality-service.service';
 
 declare var ol: any;
 declare var map: any;
@@ -20,6 +21,15 @@ export class QualityComponentComponent implements OnInit {
 
   constructor(private http: HttpClient, private service: QualityServiceService) { }
 
+  getAirService(term1: string, term2: string) {
+    this.service
+        .getAirData(term1,term2)
+        .subscribe((records: any) => {
+          console.log(records);
+          this.results = records;
+        })
+  }
+  
   ngOnInit() {
     
     this.map = new ol.Map({
@@ -51,14 +61,7 @@ export class QualityComponentComponent implements OnInit {
     });
   }
 
-  getAirService(term1: string, term2: string) {
-    this.service
-        .getAirData(term1,term2)
-        .subscribe((records: any) => {
-          console.log(records);
-          this.results = records;
-        })
-  }
+  
 }
 
 
