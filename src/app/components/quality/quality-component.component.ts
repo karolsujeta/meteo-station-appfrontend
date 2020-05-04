8bae6231a16d4db635130cb1d42fd34a9739e245
Szymon Borodziuk <szymon671@gmail.com>
2020-05-04 10:43:38 +0200

    zmiana sposobu wyswietlania informacji (wyswietlanie w miejscu klikniecia z dalaczona minka chmurki)

--- src/app/components/quality/quality-component.component.ts
+++ src/app/components/quality/quality-component.component.ts
@@ -6,13 +6,35 @@ import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { AirData } from '../../services/quality/quality-module';
 // import { QualityServiceService } from 'src/app/services/quality/quality-service.service';
 
-
+/**
+ * @ignore
+ */
 declare var ol: any;
+/**
+ * @ignore
+ */
 declare var $: any;
+/**
+ * Współrzędna odpowiadająca szerokości geograficznej.
+ */
 var latitude: any;
+/**
+ * Współrzędna odpowiadająca długości geograficznej.
+ */
 var longitude: any;
-var popupCloserEventAdded = false;
-// var results: any;
+/**
+ * Obiekt wyświetlający wybrane informacje o jakości powietrza z API Airly.
+ */
+var popupContainer: any;
+/**
+ * Przycisk zamykania popupa
+ */
+var popupCloser: any;
+/**
+ * Obiekt interpretujący wyniki w postaci obrazu (chmurki).
+ */
+var cloudContainer: any;
+
 
 @Component({
   selector: 'app-quality-component',
@@ -23,13 +45,51 @@ var popupCloserEventAdded = false;
 
 export class QualityComponentComponent implements OnInit {
 
-
+  /**
+   * Zmienna przechowująca mapę wyświetloną na stronie.
+   */
   private map;
-  //public results : AirData[] = [];
-
+/**
+   * Zmienna przechowująca odpowiedź z API Airly.
+   */
+  private results: any;
+    /**
+   * Konstruktor klasy 'QualityComponentComponent'.
+   * @param http
+   * @param service
+   */
   constructor(private http: HttpClient, private service: QualityServiceService) { }
 
+    /**
+   * Inicjowanie wyświetlenia mapy na stronie.
+   */
   ngOnInit() {
+    popupContainer = document.getElementById('popup');
+    popupCloser = document.getElementById('popup-closer');
+    cloudContainer = document.getElementById('cloudPopup');
+
+    var overlay = new ol.Overlay({
+    element: popupContainer,
+    autoPan: true,
+    autoPanAnimation: {
+      duration: 250
+    }
+  });
+  var cloudOverlay = new ol.Overlay({
+    element: cloudContainer,
+    autoPan: true,
+    autoPanAnimation: {
+      duration: 250
+    }
+  });
+  
+  popupCloser.onclick = function() {
+    overlay.setPosition(undefined);
+    cloudOverlay.setPosition(undefined);
+    popupCloser.blur();
+    return false;
+  };
+
     this.map = new ol.Map({
       target: 'map',
       layers: [
@@ -40,95 +100,29 @@ export class QualityComponentComponent implements OnInit {
       view: new ol.View({
         center: ol.proj.fromLonLat([23.1688403, 53.1324886]),
         zoom: 8,
-
-      })
+      }),
+      overlays: [overlay,cloudOverlay]
     });
 
     var serviceTmp = this.service;
+    var qualityComponent = this;
 
     this.map.on('click', function (args) {
       console.log(args.coordinate);
       var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
       console.log(lonlat);
-
       longitude = lonlat[0];
-      latitude = lonlat[1];
+      latitude = lonlat[1];      
       (`lat: ${latitude} long: ${longitude}`);
-      var mapTmp = this;
       serviceTmp
         .getAirData(latitude, longitude)
         .subscribe((records: any) => {
-          showPopup(mapTmp, records)
+          console.log(records);
+          qualityComponent.results = records;
         });
-      
+        overlay.setPosition(args.coordinate);
+        cloudOverlay.setPosition(args.coordinate);
     });
-    if (popupCloserEventAdded === false) {
-      window.addEventListener('click', closePopup);
-      popupCloserEventAdded = true;
-    }
   }
 }
 
-function closePopup(event) {
-  var element = this.document.getElementById('popup');
-  if (element === null) {
-    $('.popover').remove();
-    window.removeEventListener('click', closePopup);
-    popupCloserEventAdded = false;
-  } else if (!element.contains(<Node>event.target)){
-    $('.popover').remove();
-  }
-};
-
-function showPopup(map, qualityData) {
-  // Popup showing the position the user clicked
-  var popup = new ol.Overlay({
-    element: document.getElementById('popup'),
-  });
-  map.addOverlay(popup);
-
-  var element = popup.getElement();
-  // if ($(element).data('popover')) {
-    // $(element).popover('destroy');
-  // }
-  console.log(qualityData);
-  popup.setPosition(ol.proj.fromLonLat([longitude, latitude]));
-  $(element).popover({
-    placement: 'top',
-    animation: false,
-    html: true,
-    autoPan: true,
-    content: '<code>' +
-      qualityData[0].current.values[0].value + ' ' +
-      qualityData[0].current.values[2].value + ' ' +
-      qualityData[0].current.values[1].value + ' ' +
-      qualityData[0].current.indexes[0].value + ' ' +
-      qualityData[0].current.indexes[0].level + ' ' +
-      qualityData[0].current.indexes[0].description + '</code>'
-  });
-  $(element).popover('show');
-
-}
-
-
-
-//Na wszelki wypadek jakby trzeba bylo dodać pin (strzaleczka tam gdzie sie kliknie)
-
-    // var vectorLayer = new ol.layer.Vector({
-    //   source: new ol.source.Vector({
-    //     features: [new ol.Feature({
-    //       geometry: new ol.geom.Point(ol.proj.transform([longitude, latitude], 'EPSG:4326', 'EPSG:3857')),
-    //     })]
-    //   }),
-    //   style: new ol.style.Style({
-    //     image: new ol.style.Icon({
-    //       anchor: [0.5, 1],
-    //       anchorXUnits: "fraction",
-    //       anchorYUnits: "fraction",
-    //       scale: 0.05,
-    //       src: "assets/marker/marker.png"
-
-    //     })
-    //   })
-    // });
-    // this.map.addLayer(vectorLayer);
